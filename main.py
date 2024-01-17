fb25ff1c8d19f2a7d655b936506e84d7b1e3d23e
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2024-01-17 09:41:38 -0500

    Finished PNG Conversion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from typing import Tuple, List
 import re
 from pprint import pprint
 from pathlib import Path
@@ -24,8 +25,8 @@ class CatagorizedMedia:
 
 def get_jpg_timestamp(file_path) -> datetime | None: 
     exif = Image.open(file_path).getexif()
-    # for item in (exif.items()):
-        # print(f"{item}")
+    for item in (exif.items()):
+        print(f"{item}")
     if not exif:
         raise Exception(f'Image {file_path} does not have EXIF data.')
     if 36867 in exif.keys(): 
@@ -34,6 +35,34 @@ def get_jpg_timestamp(file_path) -> datetime | None:
         return exif[306]
     else:
         return None
+
+def get_png_timestamp(file_path) -> str | None: 
+    file = Image.open(file_path)
+    file.load()
+    exif = file.getexif()
+    # for key in exif.keys(): 
+        # print(key, exif[key])
+
+    # Try standard exif
+    if 36867 in exif.keys(): 
+        return exif[36867]
+    elif 306 in exif.keys():
+        return exif[306]
+
+    # If the file is from photoshop, we can try and get the date created 
+    # XML is a binary stream, so we need to remove some characters that mess with the regex
+    new_string = ""
+    for char in file.info['XML:com.adobe.xmp']:
+        if char != '\n':
+            new_string += char 
+    m = re.match(r'(.*)(<photoshop:DateCreated>)(.*)(<\/photoshop:DateCreated>)(.*)', new_string)
+    if m: 
+        new_string = m[3].replace("T", " ")
+        new_string = new_string.replace("-", ":")
+        print(new_string)
+        return new_string
+    
+    return None
 
 def get_heic_timestamp(file_path: str) -> str | None: 
     register_heif_opener()
@@ -61,6 +90,7 @@ def get_mp4_timestamp(file_path: str) -> str | None:
 
 TIMESTAMP_FUNCTIONS = {
         '.jpg': get_jpg_timestamp,
+        '.png': get_png_timestamp,
         '.heic': get_heic_timestamp,
         '.mp4': get_mp4_timestamp 
         }
@@ -142,7 +172,7 @@ def main():
     args = parser.parse_args()
 
     if args.file:
-        print("File was supplied")
+        # print("File was supplied")
         date_time = get_timestamp_of_media(args.file)
         if not date_time:
             print(f"Could not get datetime of {date_time}")
@@ -177,11 +207,9 @@ def main():
 
         success = []
         error = []
-        new_filenames = []
+        new_filenames: List[Tuple[str, str]] = []
 
         print("")
-        print("New file names: ")
-
         # TODO: Why do I need to get the first index here?
         for file in media_lists.files_to_convert[0]:
 
@@ -198,13 +226,16 @@ def main():
                 continue
 
             new_filename = new_filename + extension
-            print("{:<20} -> {:<20}".format(file, new_filename))
+            # print("{:<20} -> {:<20}".format(file, new_filename))
             success.append(file)
-            new_filenames.append((file, new_filename))
+            directory = os.path.dirname(file)
+            # print(directory)
+            new_filenames.append((file, os.path.join(directory, new_filename)))
 
 
         print('Files to rename: ')
-        pprint(success)
+        for old_filename, new_filename in new_filenames:
+            print("{:<20} -> {:<20}".format(os.path.split(old_filename)[1], os.path.split(new_filename)[1]))
         
         print('Failed files: ')
         pprint(error)
